12215743691d2cd09df8b4a229e7fc75f38e49c1
Levente <122294490+Levi-24@users.noreply.github.com>
2024-11-20 12:16:08 +0100

    Add project files.

---
+++ Palinka.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Palesz
+{
+    internal class Palinka
+    {
+        public int Alkohol { get; set; }
+        public string Gyumolcs { get; set; }
+        public int Mennyiseg { get; set; }
+        public int KeszitesEve { get; set; }
+        public int Ar { get; set; }
+
+        public Palinka(int alkohol, string gyumolcs, int mennyiseg, int keszitesEve, int ar)
+        {
+            Alkohol = alkohol;
+            Gyumolcs = gyumolcs;
+            Mennyiseg = mennyiseg;
+            KeszitesEve = keszitesEve;
+            Ar = ar;
+        }
+
+        public override string ToString()
+        {
+            return $"{Alkohol}% - {Gyumolcs} - Menny.:{Mennyiseg} - {KeszitesEve} - Ár:{Ar}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Palesz;
+
+List<Palinka> palinkak = new();
+string[] gyumolcsok = { "alma", "korte", "szilva", "barack", "cseresznye", "meggy", "dio", "eper", "ribizli", "citrom" };
+
+for (int i = 0; i < 20; i++)
+{
+    palinkak.Add(new Palinka(
+        alkohol: Random.Shared.Next(30, 87),
+        gyumolcs: gyumolcsok[Random.Shared.Next(gyumolcsok.Length)],
+        mennyiseg: Random.Shared.Next(0, 50),
+        keszitesEve: Random.Shared.Next(2000, DateTime.Now.Year),
+        ar: Random.Shared.Next(50, 1000)
+        ));
+}
+
+foreach (var palinka in palinkak)
+{
+    Console.WriteLine(palinka);
+}
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine($"{palinkak.Sum(p => p.Mennyiseg) / 10} liter pálinkánk van");
+
+int profit = 0;
+for (int i = 0; i < 50; i++)
+{
+    Palinka neo = palinkak[Random.Shared.Next(palinkak.Count)];
+    profit += (neo.Mennyiseg / 2) * neo.Ar;
+}
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine(profit + " Ft a profit");
+Console.ResetColor();
